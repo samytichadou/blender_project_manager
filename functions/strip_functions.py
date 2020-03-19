04f39360e5fa458f20e5da310b6b8d2fc80e9ddb
samytichadou <samytichadou@gmail.com>
2020-03-19 20:17:57 +0100

    improve strip functions

--- functions/strip_functions.py
+++ functions/strip_functions.py
@@ -1,12 +1,6 @@
 import bpy
 
 
-# return strip duration
-def returnStripStartEnd(strip):
-    start = strip.frame_start + strip.frame_offset_start
-    end = start + strip.frame_final_duration - 1
-    return start, end
-
 # check if strip is in target timing on the timeline
 def checkStripInTargetSpaceOnSequencer(start, end, target_start, target_end):
     if target_start >= start and target_start <= end:
@@ -18,14 +12,15 @@ def checkStripInTargetSpaceOnSequencer(start, end, target_start, target_end):
     else:
         return False
 
-# returne available position for a strip
+# return available position for a strip
 def returnAvailablePositionStripChannel(start, duration, sequencer):
     unavailable_channels = []
     strip_end = start + duration
 
     # get unavailable channels
     for s in sequencer.sequences_all:
-        s_start, s_end = returnStripStartEnd(s)
+        s_start = s.frame_final_start
+        s_end = s.frame_final_end - 1
         is_overlapping = checkStripInTargetSpaceOnSequencer(s_start, s_end, start, strip_end)
         if is_overlapping:
             if s.channel not in unavailable_channels:
@@ -36,3 +31,11 @@ def returnAvailablePositionStripChannel(start, duration, sequencer):
         if i not in unavailable_channels:
             return i
     return 0
+
+# return selected strips
+def returnSelectedStrips(sequencer):
+    selected_strips = []
+    for strip in sequencer.sequences_all:
+        if strip.select:
+            selected_strips.append(strip)
+    return selected_strips
