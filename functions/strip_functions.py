5a0a5b16549fab9e692ea92127ccdfb3977995fa
samytichadou <samytichadou@gmail.com>
2020-03-19 15:36:43 +0100

    clean strip functions

--- functions/strip_functions.py
+++ functions/strip_functions.py
@@ -9,21 +9,13 @@ def returnStripStartEnd(strip):
 
 # check if strip is in target timing on the timeline
 def checkStripInTargetSpaceOnSequencer(start, end, target_start, target_end):
-    print("start -------- " + str(start))
-    print("target_start - " + str(target_start))
-    print("end ---------- " + str(end))
-    print("target_end---- " + str(target_end))
     if target_start >= start and target_start <= end:
-        print("sit1")
         return True
     elif target_end >= start and target_end <= end:
-        print("sit2")
         return True
     elif start >= target_start and end <= target_end:
-        print("sit3")
         return True
     else:
-        print("sit0")
         return False
 
 # returne available position for a strip
@@ -31,19 +23,16 @@ def returnAvailablePositionStripChannel(start, duration, sequencer):
     unavailable_channels = []
     strip_end = start + duration
 
+    # get unavailable channels
     for s in sequencer.sequences_all:
         s_start, s_end = returnStripStartEnd(s)
-        print()
-        print(s.name)
         is_overlapping = checkStripInTargetSpaceOnSequencer(s_start, s_end, start, strip_end)
         if is_overlapping:
             if s.channel not in unavailable_channels:
                 unavailable_channels.append(s.channel)
 
-    print(unavailable_channels)
+    # loop through channel
     for i in range (1,32):
         if i not in unavailable_channels:
-            print(i)
             return i
-
     return 1
