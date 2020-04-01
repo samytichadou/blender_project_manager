8c33f0772e56e5885fd589d27eb8c005a8956525
samytichadou <samytichadou@gmail.com>
2020-04-01 15:49:08 +0200

    fix redrawAreas funct

--- functions/utils_functions.py
+++ functions/utils_functions.py
@@ -2,9 +2,8 @@ import bpy
 
 # redraw areas
 def redrawAreas(context, area_to_redraw):
-    for window in context.window_manager.windows:
-        for area in window.screen.areas:
-            if area.type == area_to_redraw: area.tag_redraw()
+    for area in context.screen.areas:
+        if area.type == area_to_redraw: area.tag_redraw()
 
 
 # list difference
