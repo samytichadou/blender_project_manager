d2b4f99e2204195047992573107bd35f94046a76
samytichadou <samytichadou@gmail.com>
2020-05-04 18:55:48 +0200

    asset filtering

--- asset_ui_list.py
+++ asset_ui_list.py
@@ -24,6 +24,8 @@ class BPM_UL_Asset_UI_List(bpy.types.UIList):
     # Called once to filter/reorder items.
     def filter_items(self, context, data, propname):
 
+        helper_funcs = bpy.types.UI_UL_list
+
         display = context.window_manager.bpm_generalsettings.panel_asset_display
 
         # Default return values.
@@ -33,11 +35,21 @@ class BPM_UL_Asset_UI_List(bpy.types.UIList):
         col = getattr(data, propname)
         
         ### FILTERING ###
-        if display != "ALL":
+        if self.filter_name or display != "ALL" or self.use_filter_sort_alpha:
             flt_flags = [self.bitflag_filter_item] * len(col)
 
-            for idx, asset in enumerate(col):
-                if asset.asset_type != display:
-                    flt_flags[idx] = 0
+            # name search
+            if self.filter_name :
+                flt_flags = helper_funcs.filter_items_by_name(self.filter_name, self.bitflag_filter_item, col, "name", flags=None, reverse=False)
+
+            # category search
+            if display != "ALL":
+                for idx, asset in enumerate(col):
+                    if asset.asset_type != display:
+                        flt_flags[idx] = 0
+
+            # Reorder by name
+            if self.use_filter_sort_alpha:
+                flt_neworder = helper_funcs.sort_items_by_name(col, "name")
 
         return flt_flags, flt_neworder
