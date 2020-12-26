828a946dad7a40d551bcd0d3298e0b2a472b34fb
samytichadou <samytichadou@gmail.com>
2020-12-26 18:12:57 +0100

    hide comments by default

--- properties.py
+++ properties.py
@@ -93,7 +93,7 @@ class BPMShotComments(bpy.types.PropertyGroup):
     time : bpy.props.StringProperty(name = "Creation Date")
     edit_time : bpy.props.StringProperty(name = "Edition Date")
     author : bpy.props.StringProperty(name = "Author")
-    hide : bpy.props.BoolProperty(name = "Hide Comment", defaul = True)
+    hide : bpy.props.BoolProperty(name = "Hide Comment", default = True)
 
 
 # project settings
