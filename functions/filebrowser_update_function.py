3eb004f437490bb4b3fa20d34165712397d1d8a9
samytichadou <samytichadou@gmail.com>
2021-01-20 01:44:54 +0100

    add bypass option for update function

--- functions/filebrowser_update_function.py
+++ functions/filebrowser_update_function.py
@@ -6,6 +6,9 @@ def updateFilebrowserPath(self, context):
     winman = context.window_manager
     folders_coll = winman.bpm_customfolders
     general_settings = context.window_manager.bpm_generalsettings
+
+    if general_settings.bypass_update_tag:
+        return
     
     area = context.area
 
