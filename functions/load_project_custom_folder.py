1fdb0b349c594ef98cdd92edce6cfcd31471a36f
samytichadou <samytichadou@gmail.com>
2021-01-20 02:28:47 +0100

    prevent json update when loading it

--- functions/load_project_custom_folder.py
+++ functions/load_project_custom_folder.py
@@ -14,6 +14,12 @@ def load_custom_folders(winman):
         if debug: print(no_custom_folder_file_statement) #debug
         return
 
+    general_settings = winman.bpm_generalsettings
+    general_settings.bypass_update_tag = True
+
     custom_folders_coll = winman.bpm_customfolders
     loadJsonInCollection(winman, custom_folders_file, custom_folders_coll, 'folders')
+
+    general_settings.bypass_update_tag = False
+
     if debug: print(loaded_folders_statement) #debug
