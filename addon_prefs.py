ea1f8397ceef8fcca5d9891a1be90c5196420283
samytichadou <samytichadou@gmail.com>
2023-06-05 23:48:35 +0200

    syntax

--- addon_prefs.py
+++ addon_prefs.py
@@ -50,23 +50,13 @@ class BPM_PF_addon_prefs(bpy.types.AddonPreferences):
     def draw(self, context):
         layout = self.layout
 
-        layout.prop(
-            self,
-            "preferences_folder",
-            text = "Preference Folder",
-            )
+        layout.prop(self, "preferences_folder", text = "Preference Folder")
 
         # Available Projects
         box = layout.box()
         row = box.row()
-        row.label(
-            text = "Available Pojects"
-            )
-        row.operator(
-            "bpm.reload_global_projects",
-            text = "",
-            icon = "FILE_REFRESH",
-            )
+        row.label(text = "Available Projects")
+        row.operator("bpm.reload_global_projects", text = "", icon = "FILE_REFRESH")
         global_projects = context.window_manager.bpm_global_projects
         if global_projects:
             col = box.column(align = True)
@@ -75,82 +65,40 @@ class BPM_PF_addon_prefs(bpy.types.AddonPreferences):
                 row = col.row()
                 row.label(text = p.project_name)
                 row.label(text = p.folder)
-                op = row.operator(
-                    "bpm.remove_global_project",
-                    text = "",
-                    icon = "X",
-                    )
+                op = row.operator("bpm.remove_global_project", text = "", icon = "X")
                 op.name = p.name
 
         # New Project
         row = box.row()
-        row.label(
-            text = "New Project",
-            )
-        row.prop(
-            self,
-            "new_project_name",
-            text = "",
-            )
-        row.prop(
-            self,
-            "new_project_folder",
-            text = "",
-            )
-        row.operator(
-            "bpm.create_project",
-            text = "Create",
-            )
+        row.label(text = "New Project")
+        row.prop(self, "new_project_name", text = "")
+        row.prop(self, "new_project_folder", text = "")
+        row.operator("bpm.create_project", text = "Create")
 
         # User Management
         box = layout.box()
-        box.label(
-            text = "User Management")
+        box.label(text = "User Management")
         row = box.row()
-        row.operator(
-            "bpm.create_user",
-            )
-        row.operator(
-            "bpm.remove_user",
-            )
+        row.operator("bpm.create_user")
+        row.operator("bpm.remove_user")
 
         # User Login
         # TODO specific login draw method for reuse
         box = layout.box()
         row = box.row()
-        row.label(
-            text = "Login",
-            )
+        row.label(text = "Login")
         if self.logged_user:
             user_label = self.logged_user
         else:
             user_label = "Not logged"
-        row.label(
-            text = user_label,
-            icon = "USER",
-            )
+        row.label(text = user_label, icon = "USER")
         if self.athcode:
-            row.label(
-                text = self.athcode,
-                )
-        row.operator(
-            "bpm.user_logout",
-            icon = "CANCEL",
-            )
+            row.label(text = self.athcode)
+        row.operator("bpm.user_logout", icon = "CANCEL")
         row = box.row()
-        row.prop(
-            self,
-            "user_temp",
-            )
-        row.prop(
-            self,
-            "password_temp",
-            )
-        row.operator(
-            "bpm.user_login",
-            text = "",
-            icon = "CHECKMARK",
-            )
+        row.prop(self, "user_temp")
+        row.prop(self, "password_temp")
+        row.operator("bpm.user_login", text = "", icon = "CHECKMARK")
 
 # Get addon
 def getAddon():
