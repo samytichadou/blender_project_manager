8d6f1df4aa03aa379778820392cc917129ad93c7
samytichadou <samytichadou@gmail.com>
2023-06-12 00:31:25 +0200

    poll function

--- project_management/project_menu.py
+++ project_management/project_menu.py
@@ -1,11 +1,17 @@
 import bpy
+import os
 
 from ..addon_prefs import getAddonPreferences
 
-def bpm_project_topbar(self, context):
+def poll_bpm_project(context):
     try:
-        datas = context.window_manager["bpm_project_datas"]
+        context.window_manager["bpm_project_datas"]
+        return True
     except KeyError:
+        return False
+
+def bpm_project_topbar(self, context):
+    if not poll_bpm_project(context):
         return
     if context.region.alignment == 'RIGHT':
         layout=self.layout
@@ -20,13 +26,35 @@ class BPM_MT_project_menu(bpy.types.Menu):
     bl_label = "BPMP Project"
     bl_idname = "BPM_MT_project_menu"
 
+    @classmethod
+    def poll(cls, context):
+        return poll_bpm_project(context)
+
     def draw(self, context):
         layout = self.layout
+
         layout.label(
             text = getAddonPreferences().logged_user,
             icon = "USER",
             )
+
         layout.separator()
+
+        layout.label(text = "Lock Files") # Placeholder
+
+        layout.separator()
+
+        layout.label(text = "Create Episode(s)") # Placeholder
+        layout.label(text = "Remove Episode(s)") # Placeholder
+
+        layout.separator()
+
+        layout.label(text = "Manage Users") # Placeholder
+
+        layout.separator()
+
+        layout.label(text = "Modify Project") # Placeholder
+
 
 ### REGISTER ---
 def register():
